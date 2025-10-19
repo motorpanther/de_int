b92e6a7a7f6094fb17554dbef85c867f6fa275cb
Rh Slayers <jonathanzhou1992jz@users.noreply.github.com>
2025-10-18 22:04:13 -0400

    local dbt and load to bigquery

---
+++ dbt_project/models/marts/customers.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='table') }}
+
+with customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order,
+        max(order_date) as most_recent_order,
+        count(order_id) as number_of_orders
+    from {{ ref('stg_orders') }}
+    group by customer_id
+),
+
+final as (
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order,
+        customer_orders.most_recent_order,
+        customer_orders.number_of_orders
+    from {{ ref('stg_customers') }} customers
+    left join customer_orders
+        on customers.customer_id = customer_orders.customer_id
+)
+
+select * from final
